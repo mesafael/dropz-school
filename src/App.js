fd195d5cac76966e71139cf3a40c9a8a9a3d7562
Rafael Melo <rafaelsantosmello@gmail.com>
2023-10-11 16:47:22 -0300

    add pge login and register

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Route,Routes, BrowserRouter } from "react-router-dom";
+
+import { Login } from "./Login/Login";
+import { Register } from "./Register/Register";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route component = { Login }  path="/login" />
+      <Route component = { Register }  path="/register" />
+    </Routes>
+   
+    
+</BrowserRouter>
+  );
+}
+
+export default App;
